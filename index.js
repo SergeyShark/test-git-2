8680838064dd2d306c9e4cbf5d71313b3161a801
Sergey Satanin <Satanin.krut-serega-2010@yandex.ru>
2023-01-22 16:42:12 +0600

    repeat

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 console.log('Hello world')
 
-<<<<<<< HEAD
 const userAge = 35
 
 if(userAge >= 18){
     console.log('вы совершеннолетний')
 }else{
     consolr.log('вы не совершеннолетний')
-=======
-const age = 20
-
-if(age >= 18){
-    console.log('Вы совершеннолетний')
-}else{
-    console.log('вы не совершеннолетний')
->>>>>>> feature/user-access
 }
